d37611d156738da8423132f7e942b93fb2f8c8f5
LyokoJeremie <lucheng989898@gmail.com>
2026-05-01 02:54:07 +0800

    Building_BlueprintRealizer

---
+++ FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/BlueprintRealizer.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace FullyAutomaticOmniCrafter
+{
+    // 蓝图具现器 (Blueprint Realizer)
+    // 将指定活动区内的待建造的建筑蓝图立即建造成建筑
+    // 以工作量消耗电力，优先扣除CompMatterEnergyConverterBattery的能量
+    // 断电时停止工作，停止工作时无任何性能影响
+    // 用类似于开发者模式（God Mode）的逻辑
+    public class Building_BlueprintRealizer: Building
+    {
+        
+    }
+}
