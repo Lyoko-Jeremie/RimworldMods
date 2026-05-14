f42bc72ce4706cdc782967c700ec42afae6b843e
LyokoJeremie <lucheng989898@gmail.com>
2026-05-15 03:49:22 +0800

    StatusAllocationTerminal

--- FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/StatusAllocationTerminal.cs
+++ FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/StatusAllocationTerminal.cs
@@ -17,8 +17,29 @@ namespace FullyAutomaticOmniCrafter
         }
     }
     
+    [StaticConstructorOnStartup]
+    public static class StatusAllocationTerminalTex
+    {
+        public static readonly Texture2D IconOpenDialog =
+            ContentFinder<Texture2D>.Get("UI/Commands/StatusAllocationTerminal_OpenDialog", true) ?? BaseContent.WhiteTex;
+    }
+    
     /// <summary>
-    /// 建筑弹出一个菜单，并在菜单上为指定的我方pawn手动添加或移除状态，使得pawn即使离开地图也有这个这状态
+    /// 一个可以手动为我方殖民者调整hediff的建筑
+    /// 有两个功能模式：
+    /// 1 自动光环模式
+    /// 2 手动赋予移除模式
+    ///
+    /// 手动赋予移除模式：
+    /// 建筑弹出一个菜单界面，并在菜单上为指定的我方pawn手动添加或移除状态，使得pawn即使离开地图也有这个这状态
+    /// 从左到右ABCD四栏，A是pawn名字和头像，B是选中的pawn的当前状态列表（像原版一样以部位分组），C是状态模板，D是可用状态列表。
+    /// 从D中选择状态，点击每个状态右侧的按钮弹出部位的下拉菜单，选择部位添加到C中，C中以部位为组分组显示，双击C中的条目移除状态。
+    ///
+    /// 自动光环模式：
+    /// 目的：设置赋予模板（包括hediff和对于身体部位）和移除列表，自动对当前地图上的我方人员和动物及机器人和race进行自动赋予和自动移除。
+    /// 建筑弹出一个菜单界面，在界面上设置自动光环模式的赋予参数，以及自动移除列表。
+    /// 从左到右分ABC三栏，A是自动赋予模板列表（像原版一样以部位分组），B是自动移除列表，C是可选状态列表，C中每个条目尾部有两个按钮，分别用于添加和移除状态，添加按钮点击同样弹出部位的下拉菜单，选择部位添加到C中。
+    /// 
     /// </summary>
     public class StatusAllocationTerminal : ThingComp
     {
@@ -37,7 +58,7 @@ namespace FullyAutomaticOmniCrafter
                 {
                     defaultLabel = "管理人员状态",
                     defaultDesc = "打开终端面板，为我方人员分配或移除状态。",
-                    icon = TexCommand.Install, // 使用原版的安装图标，可自定义
+                    icon = StatusAllocationTerminalTex.IconOpenDialog,
                     action = delegate ()
                     {
                         // 点击按钮后，打开我们自定义的 UI 窗口
