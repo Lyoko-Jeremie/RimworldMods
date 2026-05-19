dc69be76e7f57ac147a43ab28eccbd6e0b13c070
LyokoJeremie <lucheng989898@gmail.com>
2026-05-19 15:32:29 +0800

    .

--- FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/Patch_OmniAutoSurgeonSurgery.cs
+++ FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/Patch_OmniAutoSurgeonSurgery.cs
@@ -20,5 +20,45 @@ namespace FullyAutomaticOmniCrafter
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(RecipeWorker), "ConsumeIngredient")]
+    public static class Patch_OmniAutoSurgeonSurgery_ConsumeIngredient
+    {
+        public static bool Prefix()
+        {
+            return !OmniAutoSurgeonSurgeryContext.IsActive;
+        }
+    }
+
+    [HarmonyPatch(typeof(Thing), "Destroy")]
+    public static class Patch_OmniAutoSurgeonSurgery_ThingDestroy
+    {
+        public static bool Prefix(Thing __instance)
+        {
+            if (OmniAutoSurgeonSurgeryContext.IsActive)
+            {
+                // We prevent destruction of things during OmniAutoSurgeon operations.
+                // This covers cases where RecipeWorkers manually destroy ingredients.
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Thing), "SplitOff")]
+    public static class Patch_OmniAutoSurgeonSurgery_ThingSplitOff
+    {
+        public static bool Prefix(Thing __instance, int count, ref Thing __result)
+        {
+            if (OmniAutoSurgeonSurgeryContext.IsActive)
+            {
+                // Return the instance itself instead of splitting it, 
+                // preventing stack count reduction.
+                __result = __instance;
+                return false;
+            }
+            return true;
+        }
+    }
 }
 
