0d08380a0c467aa5660d73943cd8bee77b6bc0f9
LyokoJeremie <lucheng989898@gmail.com>
2026-06-10 01:39:47 +0800

    .

--- FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/Patch_CompBiosphere.cs
+++ FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/Patch_CompBiosphere.cs
@@ -67,8 +67,8 @@ namespace FullyAutomaticOmniCrafter
         }
     }
 
-    [HarmonyPatch(typeof(WildPlantSpawner), "CheckCellForWildPlant")]
-    public static class Patch_WildPlantSpawner_CheckCellForWildPlant
+    [HarmonyPatch(typeof(WildPlantSpawner), nameof(WildPlantSpawner.CheckSpawnWildPlantAt))]
+    public static class Patch_WildPlantSpawner_CheckSpawnWildPlantAt
     {
         public static bool Prefix(WildPlantSpawner __instance, IntVec3 c, ref bool __result)
         {
@@ -99,30 +99,7 @@ namespace FullyAutomaticOmniCrafter
         }
     }
 
-    [HarmonyPatch(typeof(GlowGrid), "GameGlowAt")]
-    public static class Patch_GlowGrid_GameGlowAt
-    {
-        public static void Postfix(GlowGrid __instance, IntVec3 c, ref float __result)
-        {
-            Map map = (Map)AccessTools.Field(typeof(GlowGrid), "map").GetValue(__instance);
-            if (map == null) return;
-
-            var biosphere = CompBiosphereManager.GetBiosphereAt(map, c);
-            if (biosphere != null)
-            {
-                if (biosphere.lightingMode == LightingMode.Sunlight)
-                {
-                    __result = Mathf.Max(__result, 1f);
-                }
-                else if (biosphere.lightingMode == LightingMode.Light)
-                {
-                    __result = Mathf.Max(__result, 0.5f);
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(GlowGrid), "GroundGlowAt")]
+    [HarmonyPatch(typeof(GlowGrid), nameof(GlowGrid.GroundGlowAt))]
     public static class Patch_GlowGrid_GroundGlowAt
     {
         public static void Postfix(GlowGrid __instance, IntVec3 c, bool ignoreSky, ref float __result)
@@ -297,7 +274,7 @@ namespace FullyAutomaticOmniCrafter
     // 真空处理 (如果有 SOS2 等 Mod，可能需要额外的 Patch)
     // 这里 Patch Room.OpenRoofCount 使其始终认为有屋顶（如果 biosphere.ensureNoVacuum）
     // 这能防止原版中的“室外”判定，从而保持氧气/温度。
-    [HarmonyPatch(typeof(Room), "OpenRoofCount", MethodType.Getter)]
+    [HarmonyPatch(typeof(Room), nameof(Room.OpenRoofCount), MethodType.Getter)]
     public static class Patch_Room_OpenRoofCount
     {
         public static void Postfix(Room __instance, ref int __result)
@@ -310,24 +287,6 @@ namespace FullyAutomaticOmniCrafter
                 if (biosphere != null && biosphere.ensureNoVacuum)
                 {
                     __result = 0; // 伪装成完全封闭
-                    return;
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(Room), "OpenRoofPercentage", MethodType.Getter)]
-    public static class Patch_Room_OpenRoofPercentage
-    {
-        public static void Postfix(Room __instance, ref float __result)
-        {
-            if (__result == 0f || __instance.Map == null) return;
-            foreach (IntVec3 cell in __instance.Cells)
-            {
-                var biosphere = CompBiosphereManager.GetBiosphereAt(__instance.Map, cell);
-                if (biosphere != null && biosphere.ensureNoVacuum)
-                {
-                    __result = 0f;
                     return;
                 }
             }
