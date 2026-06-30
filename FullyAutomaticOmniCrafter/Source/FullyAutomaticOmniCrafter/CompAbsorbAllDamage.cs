63a02ce0af43272ece3873b1afb85671175e7b50
LyokoJeremie <lucheng989898@gmail.com>
2026-07-01 01:04:10 +0800

    CompAbsorbAllDamage

---
+++ FullyAutomaticOmniCrafter/Source/FullyAutomaticOmniCrafter/CompAbsorbAllDamage.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+
+namespace FullyAutomaticOmniCrafter
+{
+    public class CompProperties_AbsorbAllDamage : CompProperties
+    {
+        public CompProperties_AbsorbAllDamage()
+        {
+            compClass = typeof(CompAbsorbAllDamage);
+        }
+    }
+
+    public class CompAbsorbAllDamage : ThingComp
+    {
+        public override void PostPreApplyDamage(ref DamageInfo dinfo, out bool absorbed)
+        {
+            absorbed = true;
+        }
+    }
+}
